dea5e43e957cb0fe22038c51be38fa7bea1ce608
jonashaker <155267717+jonashaker@users.noreply.github.com>
2024-01-02 09:29:18 -0500

    Add files via upload

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,47 +1,128 @@
-class Matrix:
+import copy
+
+class Matrix(object):
     """Rectangular table of numbers in the reals."""
     def __init__(self, matrix):
         self.matrix = matrix
-        self.columns = len(matrix)
         self.rows = len(matrix[0])
+        self.columns = len(matrix)
+
+
+    def _Drop_column(self, column):
+        for i in range(self.rows):
+            self.matrix[i].pop(column)
+        self.rows = len(self.matrix)
+        self.columns = len(self.matrix[0])
+            
+
+    def _Drop_row(self, row):
+        self.matrix.pop(row)
+        self.rows = len(self.matrix)
+        self.columns = len(self.matrix[0])
+        
+    def Get_column(self, column):
+        column_new = []
+        for i in range(self.rows):
+            column_new.append(self.matrix[i][column])
+        return column_new
+
+    def Get_row(self, row):
+        return self.matrix[row]
 
     def Print_mat(self):
         for row in self.matrix:
             print(" ".join(str(number) for number in row))
 
     def Multiply_by_const(self, c):
-        for row in self.matrix:
+        for index, row in enumerate(self.matrix):
+            self.matrix[index] = [c*x for x in row]
+
+    def Transpose(self):
+        columns_new = self.rows
+        rows_new = self.columns
+        matrix_new = [[] for _ in range(rows_new)]
+
+        for column in range(self.columns):
+            for row in range(self.rows):
+                matrix_new[row].append(self.matrix[column][row])
+
+        self.matrix = matrix_new
+        self.rows = rows_new
+        self.columns = columns_new
+            
 
     def Determinant(self):
-        pass
+        if self.columns == 1:
+            #print("hej")
+            return self.matrix[0][0]
+        else:
+            determinant = 0
+            for i in range(self.columns):
+                matrix_real = copy.deepcopy(self.matrix)
+                #print(i, self.matrix[0])
+                a_curr = self.matrix[0][i]
+                self._Drop_row(0)   
+                self._Drop_column(i)
+                determinant += (-1)**(i)*a_curr*self.Determinant()
+                self.matrix = matrix_real
+                self.rows = len(self.matrix)
+                self.columns = len(self.matrix[0])
+               
+
+        return determinant
 
     def Invert(self):
         pass
 
-    def Transpose(self):
-        pass
-    
     def Eigenvalues(self):
         pass
-    
 
     def Diagonalize(self):
         pass
 
+def Identity(size):
+    matrix = []
+    for i in range(size):
+        curr_row = []
+        for j in range(size):
+            if i == j:
+                curr_row.append(1)
+            else:
+                curr_row.append(0)
+        matrix.append(curr_row)
+    return Matrix(matrix)
 
-            
+def Zeros(rows, columns):
+    matrix = [[0 for _ in range(columns)] for _ in range(rows)]
+    return Matrix(matrix)
 
+        
 def Add(A, B):
-    pass
+    new_matrix = []
+    for row in range(A.columns):
+        new_matrix.append([sum(i) for i in zip(A.matrix[row], B.matrix[row])])
+
+    return Matrix(new_matrix)
 
 def Multiply(A,B):
-    pass
+    matrix = Zeros(A.rows, B.columns)
+    
+
+
+
+x = Matrix([[-1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(x.Determinant())
 
 
 
 
 
-x = Matrix([[1,2], [3,4]])
-x.print_mat()
+
+
+
+
+
+
+
 
 
